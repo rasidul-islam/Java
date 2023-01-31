ce041e3fe58e87d7b4655f618527173f09cdc1ae
MD RASIDUL ISLAM <43813687+rasidul-islam@users.noreply.github.com>
2023-01-31 23:05:58 +0530

    Update AddTwoNumbers.java

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,5 +1,25 @@
 public class AddTwoNumbers {
     public static void main(String[] args) {
+
+//        1st Way
+
         System.out.println(5 + 3);
+
+//        2nd Way
+        int num1, num2, sum;
+        num1 = 3;
+        num2 = 4;
+        sum = num1+num2;
+        System.out.println(sum);
+
+//        3rd Way
+
+        int num3 = 5;
+        int num4 = 4;
+        int sum1;
+        
+        sum1 = num3 + num4;
+        System.out.println(sum1);
+
     }
 }
